5378848c19fc14e9055df7c96a03b8a5a59569cf
mmanchev23 <mmanchev2003@gmail.com>
2021-08-15 18:02:49 +0300

    Added edit profile page functionallity...

--- app/views.py
+++ app/views.py
@@ -19,6 +19,7 @@ def register_view(request):
 def register_submit(request):
     if request.method == "POST":
         username = request.POST["username"]
+        email = request.POST["email"]
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
 
@@ -36,6 +37,10 @@ def register_submit(request):
 
         if not username:
             messages.error(request, "The 'Username' field can not be empty!")
+            return render(request, "app/index.html")
+
+        if not email:
+            messages.error(request, "The 'Email' field can not be empty!")
             return render(request, "app/index.html")
 
         if not password:
@@ -71,7 +76,7 @@ def register_submit(request):
             return render(request, "app/index.html")
 
         try:
-            user = Customer.objects.create_user(username, password)
+            user = Customer.objects.create_user(username, email, password)
             user.save()
             login(request, user)
             messages.success(request, "You have registered successfully!")
@@ -141,7 +146,11 @@ def logout_submit(request):
 
 # Profile Views
 def profile_view(request, username):
-    user = Customer.objects.get(username=username)
+    try:
+        user = Customer.objects.get(username=username)
+    except Customer.DoesNotExist:
+        user = None
+
     total_calories = Food.objects.filter(customer=user).aggregate(Sum('calories'))['calories__sum'] or Decimal('0.00')
     total_fat = Food.objects.filter(customer=user).aggregate(Sum('fat'))['fat__sum'] or Decimal('0.00')
     total_protein = Food.objects.filter(customer=user).aggregate(Sum('protein'))['protein__sum'] or Decimal('0.00')
@@ -159,7 +168,10 @@ def profile_view(request, username):
 
 
 def profile_settings_view(request, username):
-    user = Customer.objects.get(username=username)
+    try:
+        user = Customer.objects.get(username=username)
+    except Customer.DoesNotExist:
+        user = None
 
     context = {
         "join_date": user.date_joined.date(),
@@ -170,7 +182,74 @@ def profile_settings_view(request, username):
 
 
 def profile_edit_submit(request, username):
-    pass
+    if request.method == "POST":
+        try:
+            user = Customer.objects.get(username=username)
+        except Customer.DoesNotExist:
+            user = None
+
+        # Profile Credentials
+        username = request.POST["username"]
+        email = request.POST["email"]
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+
+        # Password Credentials
+        current_password = request.POST["current_password"]
+        new_password = request.POST["new_password"]
+        confirm_password = request.POST["confirm_password"]
+
+        # Address Credentials
+        city = request.POST["city"]
+        state = request.POST["state"]
+        country = request.POST["country"]
+
+        # Social Platforms Credentials
+        website = request.POST["website"]
+        facebook = request.POST["facebook"]
+        instagram = request.POST["instagram"]
+        twitter = request.POST["twitter"]
+        github = request.POST["github"]
+
+        # Job & Numbers Credentials
+        job = request.POST.get("job", "Null")
+        phone_number = request.POST.get("phone_number", "Null")
+        mobile_phone = request.POST.get("mobile_phone", "Null")
+
+        user.username = username
+        user.email = email
+        user.first_name = first_name
+        user.last_name = last_name
+
+        if current_password and new_password and confirm_password:
+            if user.check_password(current_password):
+                if new_password == confirm_password:
+                    user.set_password(new_password)
+                else:
+                    return messages.error(request, "Passwords should match!")
+            else:
+                return messages.error(request, "That's not your password!")
+        
+        user.city = city
+        user.state = state
+        user.country = country
+
+        user.website = website
+        user.facebook = facebook
+        user.instagram = instagram
+        user.twitter = twitter
+        user.github = github
+        user.job = job
+        user.phone_number = phone_number
+        user.mobile_number = mobile_phone
+
+        user.save()
+
+        messages.success(request, "Changes saved successfully!")
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        messages.error(request, "An error occured!")
+        return HttpResponseRedirect(reverse("index"))
 
 
 def profile_delete_view(request, username):
