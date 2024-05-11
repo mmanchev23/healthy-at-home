0f76623aba2bfca1c30c734742dd14b62d5161ab
Martin Manchev <mmanchev23@gmail.com>
2024-05-11 19:25:26 +0300

    [Feature] Client application code refactoring ... - Add Tailwind CSS, Autoprefixer and PostCSS - Add SSL certificate for local development - Add react router dom - Add Vercel analytics - Add custom home and error page - Add custom logo

---
+++ client/src/Shared/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home: React.FC = () => {
+    return (
+        <h1 className="text-3xl font-bold h-screen flex items-center justify-center">
+            Comprehensive web application designed to help you lead a healthier lifestyle from the comfort of your own home. 
+        </h1>
+    );
+}
+  
+export default Home;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { Analytics } from "@vercel/analytics/react";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./Shared/Home";
+import ErrorPage from "./Shared/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    <Analytics />
   </React.StrictMode>,
-)
+);
